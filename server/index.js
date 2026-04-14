c1e669d0944558fbe6c3dc8743be5fc5e6d3486d
Saurabh Giri <saurabh.giri@devslane.com>
2026-04-15 02:51:43 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 connectDB();
